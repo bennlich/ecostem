0864a6613c93933a735e46665d45755a180930a7
Marius Nita <m@mariusnita.com>
2013-12-27 16:51:41 -0700

    rudimentary logic using water heights; add background elevation image (using external hack for now); paus/go buttons

--- water_patches.js
+++ water_patches.js
@@ -1,6 +1,7 @@
 var WaterPatchesModel = function() {
     var DataSet = ABM.DataSet;
     var Util = ABM.util;
+    var backgroundImgCtx = null;
 
     ABM.Model.prototype.startup = function() {
         //this.elevation = DataSet.importAscDataSet("data/nldroplets.asc", function(ds) {
@@ -15,87 +16,65 @@ var WaterPatchesModel = function() {
         this.refreshPatches = true;
         this.refreshLinks = false;
 
-        var volumePerCell = 20;
-        var totalVolume = volumePerCell * this.patches.maxX * this.patches.maxY;
+        //this.anim.setRate();
 
-        this.patches.own('volume elevation capacity');
+        var volumePerCell = 20;
+
+        this.patches.own('volume elevation');
         this.patches.setDefault('volume', volumePerCell);
-        this.patches.setDefault('capacity', 0);
         this.patches.setDefault('color', [100,100,150,0.01]);
         this.elevation.toPatchVar('elevation');
 
+        /* draw the elevation map into the background canvas */
+        var img = this.elevation.toImage();
+        this.patches.installDrawing(img, backgroundImgCtx);
+
         _.each(this.patches, function(patch) {
             patch.elevation = Math.floor(patch.elevation);
-            //console.log(patch.elevation);
         });
-        var sortedByElevation = _.sortBy(this.patches, function(patch) {
-            return patch.elevation;
-        });
-        var threshold = sortedByElevation[0].elevation + 1;
-        while (totalVolume > 0) {
-            _.each(sortedByElevation, function(patch) {
-                if (patch.elevation <= threshold && totalVolume > 0) {
-                    patch.capacity += 1;
-                    totalVolume -= 1;
-                }
-            });
-            threshold += 1;
-        }
-    };
-
-    ABM.Model.prototype.transferToNeighbor = function(fromPatch, toPatch, volume) {
-        if (fromPatch.volume < volume) {
-            toPatch.volume += fromPatch.volume;
-            fromPatch.volume = 0;
-        } else {
-            fromPatch.volume -= volume;
-            toPatch.volume += volume;
-        }
     };
 
     ABM.Model.prototype.step = function() {
-        var transferVolume = 20;
         var moved = 0;
 
         _.each(this.patches, function(patch) {
-            var alpha = patch.capacity / 100;
-            if (alpha > 1)
-                alpha = 1;
-            patch.color = [100,100,150,alpha];
+            if (patch.volume > 0) {
+                var minNeighbor = _.min(patch.n, function(neighbor) {
+                    return neighbor.elevation + neighbor.volume;
+                });
+
+                var minNeighborVolume = minNeighbor.volume;
+
+                if (minNeighbor !== patch) {
+                    var transferVolumeBalancePoint = 
+                            (minNeighbor.elevation + minNeighbor.volume
+                             + patch.elevation + patch.volume) / 2;
+                    var transferVolume = patch.volume - (transferVolumeBalancePoint - patch.elevation);
+                    if (transferVolume > patch.volume)
+                        transferVolume = patch.volume;
+                    patch.volume -= transferVolume;
+                    minNeighbor.volume += transferVolume;
+                }
+
+                var alpha = patch.volume / 100;
+                if (alpha > 1)
+                    alpha = 1;
+                patch.color = [100,100,150,alpha];
+                moved++;
+            }
         });
-
-        // _.each(this.patches, function(patch) {
-        //     if (patch.volume > 0) {
-        //         var minNeighbor = _.min(patch.n, function(neighbor) {
-        //             return neighbor.elevation;
-        //         });
-
-        //         if (minNeighbor.elevation < patch.elevation) 
-        //         {
-        //             if (minNeighbor.capacity == 0
-        //                 || minNeighbor.volume + transferVolume <= minNeighbor.capacity)
-        //             {
-        //                 this.transferToNeighbor(patch, minNeighbor, transferVolume);
-        //                 moved++;
-        //             } else if (minNeighbor.volume < minNeighbor.capacity) {
-        //                 transferVolume = minNeighbor.capacity - minNeighbor.volume;
-        //                 this.transferToNeighbor(patch, minNeighbor, transferVolume);
-        //                 moved++;
-        //             }
-        //         }
-        //     }
-
-        //     var alpha = patch.volume / 100;
-        //     if (alpha > 1)
-        //         alpha = 1;
-
-        //     patch.color = [100,100,150,alpha];
-        // }.bind(this));
 
         if (moved === 0) {
             this.stop();
         }
     };
 
-    return ABM.Model;
+    function initialize(asDiv, imgDiv, patchSize, xMin, xMax, yMin, yMax) {
+        backgroundImgCtx = document.getElementById(imgDiv).getContext("2d");
+        return new ABM.Model(asDiv, patchSize, xMin, xMax, yMin, yMax).debug().start();
+    }
+
+    return {
+        initialize: initialize
+    };
 }();

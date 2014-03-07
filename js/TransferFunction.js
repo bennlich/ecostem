b5bdd67e4b7b95daa1d33edb168d5d039d48d397
bennlich <bennlich@gmail.com>
2014-03-06 18:04:42 -0800

    Reverted TransferFunction to use spline instead of polynomial. Also hardcoded a min distance between control points to make curves tend to be functions.

--- js/TransferFunction.js
+++ js/TransferFunction.js
@@ -51,69 +51,69 @@ function TransferFunction(domain, domainUnit, range, rangeUnit, title, svgcanvas
 
     transfer.render = function() {
 	
-	//this.interpolate = Smooth(this.controlPoints, this.interpolatorOpts);
-	//
-	//  Curve fitting instead of smoother. It outputs a function. 
-	//    This has it's own problems, especially when there are more than like 5 control points
-	//
-	var polynomialDegree = this.controlPoints.length - 1; //matches every point exactly when degree is length-1
-	this.polynomialCoeffients = matf.compute_coefficients(this.controlPoints, polynomialDegree);
-	this.interpolate = function(x) {
-	    //res = cc[0] + cc[1]*x + cc[2]*x*x ...
-	    var cc = this.polynomialCoeffients;
-	    var res = 0;
-	    for (var i = 0; i < cc.length; i++) {
-			res += Math.pow(x,i) * cc[i];
-	    }
-	    return [x, Math.min( range[1], Math.max(range[0], res))];
-	};
+		this.interpolate = Smooth(this.controlPoints, this.interpolatorOpts);
+		//
+		//  Curve fitting instead of smoother. It outputs a function. 
+		//    This has it's own problems, especially when there are more than like 5 control points
+		//
+		// var polynomialDegree = this.controlPoints.length - 1; //matches every point exactly when degree is length-1
+		// this.polynomialCoeffients = matf.compute_coefficients(this.controlPoints, polynomialDegree);
+		// this.interpolate = function(x) {
+		//     //res = cc[0] + cc[1]*x + cc[2]*x*x ...
+		//     var cc = this.polynomialCoeffients;
+		//     var res = 0;
+		//     for (var i = 0; i < cc.length; i++) {
+		// 		res += Math.pow(x,i) * cc[i];
+		//     }
+		//     return [x, Math.min( range[1], Math.max(range[0], res))];
+		// };
 
 
-	this.indexToXCoord.domain([0, this.pathData.length - 1]);
-	for (var i = 0; i < this.pathData.length; i++) {
-	    this.pathData[i] = this.interpolate(this.indexToXCoord(i));
-	}
+		this.indexToXCoord.domain([0, this.pathData.length - 1]);
+		for (var i = 0; i < this.pathData.length; i++) {
+		    this.pathData[i] = this.interpolate(this.indexToXCoord(i));
+		}
 
-	// TODO: Make the lookup table actually a lookup table
-	// (instead of an array of samples to search through for the correct value)
-	this.indexToXCoord.domain([0, this.lookupTable.length - 1]);
-	for (var i = 0; i < this.lookupTable.length; i++) {
-	    this.lookupTable[i] = this.interpolate(this.indexToXCoord(i));
-	}
+		// TODO: Make the lookup table actually a lookup table
+		// (instead of an array of samples to search through for the correct value)
+		this.indexToXCoord.domain([0, this.lookupTable.length - 1]);
+		for (var i = 0; i < this.lookupTable.length; i++) {
+		    this.lookupTable[i] = this.interpolate(this.indexToXCoord(i));
+		}
 
-	this.path
-	    .datum(this.pathData)
-	    .attr('d', this.pathGenerator);
+		this.path
+		    .datum(this.pathData)
+		    .attr('d', this.pathGenerator);
 
-	// for inspecting the distribution of interpolated points
-	// this.container.selectAll('circle.test')
-	// 	.data(this.pathData)
-	// 		.attr('cx', function(d) { return transfer.xScale(d[0]); })
-	// 		.attr('cy', function(d) { return transfer.yScale(d[1]); })
-	// 		// .attr('cy', function(d) { return 80; })
-	// 	.enter().append('circle')
-	// 		.attr('class', 'test')
-	// 		.attr('r', 1)
-	// 		.attr('cx', function(d) { return transfer.xScale(d[0]); })
-	// 		.attr('cy', function(d) { return transfer.yScale(d[1]); });
-	// 		// .attr('cy', function(d) { return 80; });
+		// for inspecting the distribution of interpolated points
+		// this.container.selectAll('circle.test')
+		// 	.data(this.pathData)
+		// 		.attr('cx', function(d) { return transfer.xScale(d[0]); })
+		// 		.attr('cy', function(d) { return transfer.yScale(d[1]); })
+		// 		// .attr('cy', function(d) { return 80; })
+		// 	.enter().append('circle')
+		// 		.attr('class', 'test')
+		// 		.attr('r', 1)
+		// 		.attr('cx', function(d) { return transfer.xScale(d[0]); })
+		// 		.attr('cy', function(d) { return transfer.yScale(d[1]); });
+		// 		// .attr('cy', function(d) { return 80; });
 
-	var controlGroup = this.container.selectAll('g.control')
-		.data(this.controlPoints);
-	controlGroup.select('circle.control')
-	    .attr('cx', function(d) {
-			return transfer.xScale(d[0]);
-	    })
-	    .attr('cy', function(d) {
-			return transfer.yScale(d[1]);
-	    });
-	controlGroup.select('circle.handle')
-	    .attr('cx', function(d) {
-			return transfer.xScale(d[0]);
-	    })
-	    .attr('cy', function(d) {
-			return transfer.yScale(d[1]);
-	    });
+		var controlGroup = this.container.selectAll('g.control')
+			.data(this.controlPoints);
+		controlGroup.select('circle.control')
+		    .attr('cx', function(d) {
+				return transfer.xScale(d[0]);
+		    })
+		    .attr('cy', function(d) {
+				return transfer.yScale(d[1]);
+		    });
+		controlGroup.select('circle.handle')
+		    .attr('cx', function(d) {
+				return transfer.xScale(d[0]);
+		    })
+		    .attr('cy', function(d) {
+				return transfer.yScale(d[1]);
+		    });
     };
 
     var width = $('#' + svgcanID).width(),
@@ -200,22 +200,22 @@ function TransferFunction(domain, domainUnit, range, rangeUnit, title, svgcanvas
 	    .append('g')
 	    .attr('class', 'control')
 	    .call(d3.behavior.drag().on('drag', function(d, i) {
-			var leftBound = domain[0],
-			rightBound = domain[1];
+			var leftBound = transfer.xScale(domain[0]),
+			rightBound = transfer.xScale(domain[1]);
 			// TODO: Change the left and right bounds to ensure that
 			// the user-generated spline is always a function
 			// (i.e. no two y values corresponding to the same x value)
 			if (i > 0) {
-				leftBound = transfer.controlPoints[i - 1][0]+2;
+				leftBound = transfer.xScale(transfer.controlPoints[i - 1][0])+40;
 			}
 			if (i < transfer.controlPoints.length - 1) {
-				rightBound = transfer.controlPoints[i + 1][0]-2;
+				rightBound = transfer.xScale(transfer.controlPoints[i + 1][0])-40;
 			}
 			d[1] = transfer.yScale.invert(d3.event.y);
 			d[1] = Math.min(range[1], Math.max(range[0], d[1]));
 			if (i >= 0 && i < transfer.controlPoints.length) { // don't allow extents to move towards center
-				d[0] = transfer.xScale.invert(d3.event.x);
-				d[0] = Math.min(rightBound, Math.max(leftBound, d[0]));
+				d[0] = Math.min(rightBound, Math.max(leftBound, d3.event.x));
+				d[0] = transfer.xScale.invert(d[0]);
 			}
 			transfer.render();
 		}));
